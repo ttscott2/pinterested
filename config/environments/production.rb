852c07180ace9504b9cdaf9c8d646e45a9bf1baf
ttscott2 <timothytscott3@gmail.com>
2016-02-21 22:44:27 -0600

    add devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  ### COME BACK TO THIS LATER! 
+  # config.action_mailer.default_url_options = { host: 'pinterested99.herokuapp.com' }
 end
